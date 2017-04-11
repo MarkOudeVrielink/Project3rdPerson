4ec1624ab7bc3bafdcb1a5d1c5a955a162430515
Noixas <acmrz@outlook.com>
2017-04-11 18:18:23 +0200

    Player Material affected by light again...

--- Engine/Engine/assets/mge/shaders/player.fs
+++ Engine/Engine/assets/mge/shaders/player.fs
@@ -8,20 +8,137 @@ uniform bool		charged;
 
 in vec2 			texCoord;
 
-out vec4 			fragment_color;
 
-vec4 pixelColor =  texture(textureDiffuse,texCoord);
+//LIGHTNING implementation
+const int NUMBEROFLIGHTS = 4;
+
+struct lightInfo{
+    vec3 lightPosition;
+    vec3 lightDirection;
+    vec3 lightColor;
+    vec3 globalAmbient;
+    vec3 lightAttenuation;
+    float innerCone;
+    float outerCone;
+    int lightType;
+};
+uniform mat4 eyeMatrix;//Inverse View
+uniform vec4 specularColor;
+uniform float shininess;
+uniform lightInfo lights[4];
+vec3 eyePosition = vec3(eyeMatrix[3]);
+float diffuseIntensity;
+
+in vec3 worldPosition;
+in vec3 worldNormal;
+
+out vec4 fragment_color;
+vec4 CalculateLights();
+vec4 CalculateDirectional(lightInfo pLight);
+vec4 CalculatePoint(lightInfo pLight);
+vec4 CalculateSpot(lightInfo pLight);
+
+
+vec4 pixelColor;
 
 void main( void ) {
 
+    pixelColor = CalculateLights()*  texture(textureDiffuse,texCoord);
 	if(invulnerable){
 		pixelColor.a *= (1 + sin(time));
-	}	
-	
-	//if(charged){
-		//pixelColor.rg *= (1+ sin(time/2));
-		//clamp(pixelColor.rg, 0.3f, 1.0f);		
-	//}
-	
+	}
 	fragment_color = pixelColor;
 }
+
+vec4 CalculateLights() {
+    vec4 outColor;
+	for(int i = 0; i < NUMBEROFLIGHTS; i++){
+		if(lights[i].lightType == 0){
+			outColor += CalculateDirectional(lights[i]);
+		} else if(lights[i].lightType == 1){
+			outColor += CalculatePoint(lights[i]);
+		} else{
+			outColor += CalculateSpot(lights[i]);
+		}
+	}
+	return outColor;
+}
+vec4 CalculateDirectional(lightInfo pLight){
+	diffuseIntensity = max (0, dot( normalize (worldNormal),normalize(-pLight.lightDirection)));
+
+    vec3 ambientTerm =clamp( pLight.globalAmbient,0,1);
+    vec3 diffuseTerm = clamp(diffuseIntensity * pLight.lightColor,0,1) ;
+    vec3 specularTerm = vec3(0.0, 0.0, 0.0);
+
+    vec3 vertexToEye = normalize(eyePosition - worldPosition);
+    vec3 lightReflect = normalize(reflect(pLight.lightDirection,normalize(worldNormal)));
+	float specularFactor = dot(vertexToEye, lightReflect);
+
+	if (specularFactor > 0.0) {
+	    specularFactor = pow(specularFactor,32.5f);//add Specular Power
+		specularTerm = pLight.lightColor*shininess*specularFactor;
+	}
+   return vec4 (ambientTerm + diffuseTerm+specularTerm , 1);
+}
+
+vec4 CalculatePoint(lightInfo pLight){
+	float c1 = pLight.lightAttenuation.x;
+	float c2 = pLight.lightAttenuation.y;
+	float c3 = pLight.lightAttenuation.z;
+
+	diffuseIntensity = max (0, dot(-normalize(worldPosition - pLight.lightPosition), normalize (worldNormal)));
+
+	float lightDistance = distance(worldPosition, pLight.lightPosition);
+	float attenuation = 1 / (c1 + c2 * lightDistance + c3 * lightDistance*lightDistance);
+
+    vec3 ambientTerm = pLight.globalAmbient ;
+    vec3 diffuseTerm = diffuseIntensity * pLight.lightColor;
+    vec3 specularTerm = vec3(0.0, 0.0, 0.0);
+
+    vec3 vertexToEye = normalize(eyePosition - worldPosition);
+    vec3 lightReflect = normalize(reflect(pLight.lightDirection,normalize(worldNormal)));
+	float specularFactor = dot(vertexToEye, lightReflect);
+
+	if (specularFactor > 0.0) {
+	    specularFactor = pow(specularFactor,32.5f);//add Specular Power
+		specularTerm = pLight.lightColor*shininess*specularFactor;
+	}
+	ambientTerm *= attenuation;
+	diffuseTerm *= attenuation;
+	specularTerm *= attenuation;
+
+   return vec4 (ambientTerm + diffuseTerm + specularTerm, 1);
+}
+
+vec4 CalculateSpot(lightInfo pLight){
+
+    float c1 = pLight.lightAttenuation.x;
+	float c2 = pLight.lightAttenuation.y;
+	float c3 = pLight.lightAttenuation.z;
+    //attenuation based on distance light-object
+	float lightDistance = distance(worldPosition, pLight.lightPosition);
+	float attenuation = 1 / (c1 + c2 * lightDistance + c3 * lightDistance*lightDistance);
+
+	vec3 direction = normalize(pLight.lightPosition - worldPosition);
+	float angle = dot(direction, normalize(-pLight.lightDirection));
+
+	float intensity = clamp((angle - pLight.outerCone)/(pLight.innerCone - pLight.outerCone), 0.0f, 1.0f);
+
+    vec3 ambientTerm = pLight.globalAmbient ;
+    vec3 diffuseTerm =  pLight.lightColor ;
+    vec3 specularTerm = vec3(0.0, 0.0, 0.0);
+
+    vec3 vertexToEye = normalize(eyePosition - worldPosition);
+    vec3 lightReflect = normalize(reflect(pLight.lightDirection,normalize(worldNormal)));
+	float specularFactor = dot(vertexToEye, lightReflect);
+
+	if (specularFactor > 0.0) {
+	    specularFactor = pow(specularFactor,32.5f);//add Specular Power
+		specularTerm = pLight.lightColor*shininess*specularFactor;
+	}
+	diffuseTerm 	*= intensity*attenuation;
+	specularTerm 	*= intensity*attenuation;
+
+   return vec4 (ambientTerm + diffuseTerm + specularTerm, 1);
+}
+
--- Engine/Engine/src/mgengine/Materials/PlayerMaterial.h
+++ Engine/Engine/src/mgengine/Materials/PlayerMaterial.h
@@ -4,6 +4,8 @@
 #include "mge/core/ShaderProgram.hpp"
 #include "mge/core/Texture.hpp"
 #include "mge/materials/AbstractMaterial.hpp"
+#include "LightInfo.h"
+#include "Light.h"
 
 class PlayerMaterial : public AbstractMaterial
 {
@@ -28,15 +30,42 @@ private:
 	static GLint _uInvulnerable;
 	static GLint _uCharged;
 
+	//MVP
+	static GLint _modelMatrixIndex;
+	static GLint _viewMatrixIndex;
+	static GLint _perspectiveMatrixIndex;
+	//Colors
+	static GLint _globalAmbientIndex;
+	static GLint _diffuseMaterialColorIndex;
+	//Light Info
+	static GLint _lightPositionIndex;
+	static GLint _lightDirectionIndex;
+	static GLint _lightColorIndex;
+	static GLint _lightAttenuationIndex;
+	//Specular
+	static GLint _eyeMatrixIndex;
+	static GLint _specularColorIndex;
+	static GLint _specularShininessIndex;
+	//Spot Light Cone
+	static GLint _innerConeIndex;
+	static GLint _outerConeIndex;
+
+	static GLint _lightTypeIndex;
 	static GLint _aVertex;
 	static GLint _aNormal;
 	static GLint _aUV;
 
+	glm::vec3 _specularColor = glm::vec3(1, 1, 1);
+	float _specularShininess = 0;
 	float _time = 0;
 	float _blinkRate;
 
 	bool _isInvulnerable;
 	bool _isCharged;
+
+	Light* _light;
+	std::vector<Light*> _lights;
+	vector<LightInfo>*  _lightInfo = new vector<LightInfo>();
 
 	PlayerMaterial(const PlayerMaterial&);
 	PlayerMaterial& operator=(const PlayerMaterial&);
